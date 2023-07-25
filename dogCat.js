18ee57effb5a2dfbb186ad3258c753dc7701418d
SEBPO-MasudurRahman <115811230+SEBPO-MasudurRahman@users.noreply.github.com>
2023-07-26 02:40:51 +0600

    Add files via upload
    Demo Design Almost Completed

---
+++ dogCat.js
@@ -0,0 +1,108 @@
+
+      /**----------------------
+         * product scroller js ---
+         --------------------------*/
+      // tab  carouseljs
+      // initialize product scroller
+      advanceArrows("citrusX-Dog-Product");
+      function advanceArrows(idx) {
+        var splideElement = "#" + idx;
+        var splideDefaultOptions = {
+          arrows: true,
+          type: "slide",
+          start: 0,
+          focus: 0,
+          pagination: false,
+          rewindSpeed: 500,
+          speed: 500,
+          pauseOnHover: true,
+          perPage: 6,
+          perMove: 1,
+          omitEnd: true,
+          breakpoints: {
+            375: {
+              perPage: 1,
+            },
+            576: {
+              perPage: 1,
+            },
+            991: {
+              perPage: 3,
+            },
+            992: {
+              perPage: 3,
+            },
+            1024: {
+              perPage: 4,
+            },
+            1200: {
+              perPage: 5,
+            },
+            1440: {
+              perPage: 6,
+            },
+          },
+        };
+        new Splide(splideElement, splideDefaultOptions).mount();
+      }
+
+      /* ************************************************************************************** */
+      //for tab sweater & loader added
+      document.addEventListener("DOMContentLoaded", function () {
+        filterDogCatProducts("citrusX-Dog-Product");
+      });
+      // for dog filter function
+      function filterDogCatProducts(c) {
+        var x, i;
+        x = document.getElementsByClassName("citrusXDogCat__Product");
+        if (c == "all") c = "";
+        for (i = 0; i < x.length; i++) {
+          RemoveClass(x[i], "citrusTabShow");
+          if (x[i].className.indexOf(c) > -1) AddClass(x[i], " citrusTabShow");
+        }
+        advanceArrows(c);
+      }
+      function AddClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+          if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+          }
+        }
+      }
+
+      function RemoveClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+          while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+          }
+        }
+        element.className = arr1.join(" ");
+      }
+
+      // Add active class to the current button (highlight it)
+      document.addEventListener("DOMContentLoaded", function () {
+        // for dog active class filter
+        var dogBtnContainer = document.getElementById("citrusXDogCat_ID");
+        var dogBtns =
+          dogBtnContainer.getElementsByClassName("citrusXDogCat__tab_item");
+        addActiveClass(dogBtns, "citrusXDogCat__active_tab");
+      });
+      function addActiveClass(element, activeClass) {
+        for (var i = 0; i < element.length; i++) {
+          element[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName(activeClass);
+            current[0].className = current[0].className.replace(
+              " " + activeClass,
+              ""
+            );
+            this.className += " " + activeClass;
+          });
+        }
+      }
+      //for Dog best seller tab switcher
